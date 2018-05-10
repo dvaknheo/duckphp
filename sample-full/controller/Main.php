f27898f1e69ccff15f3f83c5e903c96d3f5164e4
dvakheo <dvaknheo@gmail.com>
2018-05-10 21:31:35 +0800

    继续完成注册部分

--- sample-full/controller/Main.php
+++ sample-full/controller/Main.php
@@ -1,20 +1,53 @@
 <?php
-
 class DnController
 {
+	public function __construct()
+	{
+		//把错误处理都放在这里。
+	}
 	public function index()
 	{
-	
 		$page=isset($_GET['page'])?$_GET['page']:1;
 		$page=intval($page);
 		$page=$page<1?1:$page;
 		$data=ArticleService::G()->getRecentArticle($page);
-		var_dump($data);
+		$user=SessionService::G()->getCurrentUser();
+		$data['user']=$user;
+		
+		$data['url_reg']=URL('/reg');
+		$data['url_login']=URL('/login');
+		$data['url_logout']=URL('/logout');
 		DNView::Show('main',$data);
 		
 	}
-	public function i()
+	public function reg()
 	{
-		phpinfo();
+		$data=array();
+		DNView::Show('user/reg',$data);
+	}
+	public function login()
+	{
+		$data=array();
+		DNView::Show('user/login',$data);
+	}
+	public function logout()
+	{
+		SessionService::G()->logout();
+		
+		DNView::return_route_to('/');
+	}
+	
+	public function do_reg()
+	{
+		$user=UserService::G()->reg($_POST['username'],$_POST['password']);
+		SessionService::G()->setCurrentUser($user);
+		DNView::return_route_to('/');
+	}
+	public function do_login()
+	{
+		$user=UserService::G()->login($_POST['username'],$_POST['password']);
+		SessionService::G()->setCurrentUser($user);
+		
+		DNView::return_route_to('/');
 	}
 }
--- sample-full/view/main.php
+++ sample-full/view/main.php
@@ -2,6 +2,13 @@
 <html>
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <body>
+<?php if($user){ ?>
+	欢迎你 <?=$user['username']?> 
+	<a href="<?=$url_logout?>">登出</a>
+<?php }else {?>
+	<a href="<?=$url_login?>">登录</a>
+	<a href="<?=$url_reg?>">注册</a>
+<?php } ?>
 <h1>完全性测试.</h1>
 <fieldset>
 <legend>基础测试</legend>
