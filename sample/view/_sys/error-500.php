913f54b21d71ff5cb9658c7eeebd019d97857370
dvakheo <dvaknheo@gmail.com>
2018-07-25 09:00:31 +0800

    调整报错的 view

--- sample/view/_sys/error-500.php
+++ sample/view/_sys/error-500.php
@@ -1,4 +1,5 @@
 500
+<?php if(DN::G()->isDev()){ ?>
 <fieldset>
 	<legend>Error(<?=get_class($ex);?>:<?php echo($ex->getCode());?>)</legend>
 	<?php echo($ex->getMessage());?>
@@ -10,3 +11,4 @@
 <?php echo($trace);?>
 </pre>
 </fieldset>
+<?php }?>
