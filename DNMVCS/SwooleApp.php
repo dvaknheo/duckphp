f57c71663eaa2a672359949d2400c29632c59e8f
dvakheo <dvaknheo@gmail.com>
2018-08-30 10:45:07 +0800

    基本完成，有bug ，先保存

--- DNMVCS/SwooleApp.php
+++ DNMVCS/SwooleApp.php
@@ -1,95 +1,123 @@
 <?php
 namespace DNMVCS;
+require_once(__DIR__.'/DNMVCSExt.php');
+
 /////// 用于 swoole;还要多测试。
-class SwooleApp extends DNMVCS
+class SwooleRequest // extends \swoole_http_request
 {
-	public function onShow404()
+	use DNSingleton;
+	use DNWrapper;
+	public $init_server;
+	public function init()
 	{
-		echo '404';
-		//不能用 header 了。 
+		$this->init_server=$_SERVER;
+		foreach($this->obj->server as $k=>$v){
+			$_SERVER[strtoupper($k)]=$v;
+		}
+		$_GET=$this->obj->get??[];
+		$_POST=$this->obj->post??[];
+		$_REQUEST=array_merge($_GET,$_POST);
+		
 	}
-	protected function initExceptionManager()
+	public function cleanUp()
 	{
-		//关闭默认处理。stop default feature
-		return;
+		$_SERVER=$this->init_server;
+		$_GET=[];
+		$_POST=[];
+		$_REQUEST=[];
+		// cookie, session  and other super globals
+	}
+}
+class SwooleResponse // extends \swoole_http_response
+{
+	use DNSingleton;
+	use DNWrapper;
+	public function write($str)
+	{
+		return $this->obj->write($str);
+	}
+	public static function init()
+	{
+		ob_start(function($str){
+		SwooleResponse::G()->write($str); });
+	}
+	public static function cleanUp()
+	{
+		ob_end_flush();
+	}
+}
+class SwooleApp
+{
+	use DNSingleton;
+	
+	public $onDoRequest=null;
+	protected function doRequestRun($req,$res)
+	{
+		SwooleRequest::G(SwooleRequest::W($req));
+		SwooleResponse::G(SwooleResponse::W($res));
+		SwooleResponse::G()->init();
+		SwooleRequest::G()->init();
+		if($this->onDoRequest){
+			$ret=($this->onDoRequest)($req,$res);
+			$this->doRequestCleanUp();
+			return $ret;
+		}
+		//DNRoute::G()->swoolehook();
+		DNMVCS::G()->initRoute(DNRoute::G());
+		DNRoute::G()->run();
+		$this->doRequestCleanUp();
+	}
+	protected function doRequestCleanUp()
+	{
+		DNView::G()->cleanUp();
+		DNRoute::G()->cleanUp();
+		
+		SwooleRequest::G()->cleanUp();
+		SwooleResponse::G()->cleanUp();
+		
+		SwooleRequest::G(SwooleRequest::W(new \stdClass)); // cleanup
+		SwooleResponse::G(SwooleResponse::W(new \stdClass)); //cleanup
+	}
+	protected function doRequestException(\Throwable  $ex)
+	{
+		DNMVCS::G()->onException($ex);
+		$this->doRequestCleanUp();
 	}
 	public function onRequest($req,$res)
 	{
+		//ob_start(function($str)use($res){ $res->write($str); });
 		try{
-			SwooleRoute::G(new SwooleRoute()); // 不复用
-			SwooleRoute::G()->initSwoole($req,$res);
-			DNRoute::G(SwooleRoute::G());
-			DNMVCS::G()->initRoute(DNRoute::G());
-			
-			SwooleRoute::G()->run();
-
+			$this->doRequestRun($req,$res);
 		}catch(\Throwable  $ex){
-			self::G()->onException($ex);
+			$this->doRequestException($ex);
 		}
+		//ob_end_flush();
+	}
+	public static function BindSwooleHttpServer($server,$options)
+	{
+		DNRoute::G(SwooleRoute::G());
+		DNMVCS::G()->init($options);
+		$server->on('request',[self::G(),'onRequest']);
 	}
 	public static function RunSwooleQuickly($server,$options,$file='')
 	{
-		DNMVCS::G(SwooleApp::G())->init($options);
-		$server->on('request',
-			function($req,$res)use($options,$file){
-				ob_start(function($str)use($res){
-					$res->write($str);
-				});
-				if(!$file){
-					self::G()->onRequest($req,$res);
-				}else{
-					include($file);
-				}
-				ob_end_flush();
-				$res->end();
-			}
-		);
-		$server->start();
-	}
-	public static function RunSwooleQuicklyx($server,$options)
-	{
 		self::BindSwooleHttpServer($server,$options);
+		if($file!=''){
+			self::G()->onDoRequest=function($req,$res)use($file){
+				include($file);
+			};
+		}
 		$server->start();
 	}
-
 }
 
-class SwooleRoute extends \DNMVCS\DNRoute
+class SwooleRoute extends DNRoute
 {
-	public $req_server;
-	public $req_get;
-	public $req_post;
-	
-	public function init($options)
+	public function swoolehook()
 	{
-		parent::init($options);
 		$this->path_info=$this->_SERVER('PATH_INFO')??'';
 		$this->request_method=$this->_SERVER('REQUEST_METHOD')??'';
-		$this->path_info=ltrim($this->path_info,'/');
-	}
-	public function initSwoole($req,$res)
-	{
-		$this->req_server=$req->server;
-		$this->req_get=$req->get;
-		$this->req_post=$req->post;
-	}
-	public function _SERVER($key)
-	{
-		$key=strtolower($key);
-		$a=$this->req_server;
-		return  $a[$key]??null;
-	}
-	public function _GET($key)
-	{
-		return $this->req_get[$key]??null;
-	}
-	public function _POST($key)
-	{
-		return $this->req_post[$key]??null;
-	}
-	public function _REQUEST($key)
-	{
-		return $this->_POST($key)??$this->_GET($key);
+		return $this;
 	}
 	protected function includeControllerFile($file)
 	{
