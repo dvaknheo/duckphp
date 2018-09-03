0f001a728334fc25a171339b639d43f9ad706589
dvakheo <dvaknheo@gmail.com>
2018-09-03 10:47:27 +0800

    运行通过

--- DNMVCS/SwooleAppBase.php
+++ DNMVCS/SwooleAppBase.php
@@ -2,6 +2,7 @@
 namespace DNMVCS;
 use \DNMVCS\DNMVCS as DN;
 use \DNMVCS\SuperGlobal\SuperGlobalBase;
+use \DNMVCS\SuperGlobal;
 
 class RouteWithSuperGlobal extends DNRoute
 {
@@ -65,6 +66,19 @@ class SwooleSuperGlobalCookie extends SuperGlobalBase
 		$this->data=$request->cookie??[];
 	}
 }
+class RouteRewriteHookWithSuperGlobal extends RouteRewriteHook
+{
+	use DNSingleton;
+	protected $rewriteMap=[];
+	protected function mergeHttpGet($get)
+	{
+		//$_GET=array_merge($get,$_GET??[]);
+		$data=array_merge($get,HTTP_GET::All());
+		foreach($data as $k=>$v){
+			HTTP_GET::Set($k,$v);
+		}
+	}
+}
 class SwooleReuseRouteHook
 {
 	public static function hook($route){
@@ -78,13 +92,15 @@ class CoroutineSingleton
 {
 	public static function G($object=null,$class)
 	{
-
+		if($object===null){
+			$me=DNSingletonStaticClass::$_instances[$class]??null;
+			if($me){return $object;}
+		}
 		$cid = \Swoole\Coroutine::getuid();
-		if($cid<0){return null;}
-		if($cid===0){
+		if($cid<=0){
 			if($object){
-			DNSingletonStaticClass::$_instances[$class]=$object;
-			return $object;
+				DNSingletonStaticClass::$_instances[$class]=$object;
+				return $object;
 			}
 			$me=DNSingletonStaticClass::$_instances[$class]??null;
 			if(null===$me){
@@ -94,31 +110,54 @@ class CoroutineSingleton
 			return $me;
 		}
 		
-		$key="cid-$cid";
-		DNSingletonStaticClass::$_instances[$key]=self::$_instances[$key]??[];
+		$key="cid=$cid";
+		DNSingletonStaticClass::$_instances[$key]=DNSingletonStaticClass::$_instances[$key]??[];
 		if($object){
 			DNSingletonStaticClass::$_instances[$key][$class]=$object;
 			return $object;
 		}
 		$me=DNSingletonStaticClass::$_instances[$key][$class]??null;
 		if(null===$me){
-			$object=DNSingletonStaticClass::[$class]??null;
-			if(null!==$object){return $object;}
-		
+			$master=DNSingletonStaticClass::$_instances[$class]??null;
+			if(null!==$master){return $master;}
 			$me=new $class();
 			DNSingletonStaticClass::$_instances[$key][$class]=$me;
 		}
 		return $me;
 		
 	}
+	public static function HasSingletonOutSide($class)
+	{
+		$me=DNSingletonStaticClass::$_instances[$class]??null;
+		return $me?true:false;
+	}
+	public static function Dump()
+	{
+		$cid = \Swoole\Coroutine::getuid();
+fwrite(STDERR,"====CoroutineSingletonList cid-{$cid}====".";\n");
+		$t=DNSingletonStaticClass::$_instances;
+		foreach($t as $class=>$v){
+			if(!is_array($v)){
+fwrite(STDERR,"+ ".$class.";\n");
+			}else{
+				foreach($v as $class2=>$vv){
+fwrite(STDERR,"-- $class ~ ".$class2.";\n");
+				}
+			}
+		}
+	}
 	public static function CleanCoroutineSingleton()
 	{
 		$cid = \Swoole\Coroutine::getuid();
-		if($cid<=0){return null;}
+		if($cid<=0){return;}
 		$key="cid-$cid";
-//fwrite(STDERR,var_export(array_keys(self::$_instances[$key]),true));
 		DNSingletonStaticClass::$_instances[$key]=[];
 	}
+}
+class T
+{
+	use DNSingleton;
+	public function foo(){}
 }
 class SwooleAppBase extends DNMVCS
 {
@@ -136,13 +175,13 @@ class SwooleAppBase extends DNMVCS
 	{
 		$request=$this->options['request'];
 		
-		SERVER::G(SwooleSuperGlobalServer::G())->init($request);
-		HTTP_GET::G(SwooleSuperGlobalGet::G())->init($request);
-		HTTP_POST::G(SwooleSuperGlobalPost::G())->init($request);
-		HTTP_REQUEST::G(SwooleSuperGlobalRequest::G())->init($request);
-		COOKIE::G(SwooleSuperGlobalCookie::G())->init($request);
-		SERVER::Set('DOCUMENT_ROOT',rtrim($this->options['path'],'/'));
-		SERVER::Set('SCRIPT_FILENAME',$this->options['path'].'index.php');
+		SuperGlobal\SERVER::G(SwooleSuperGlobalServer::G())->init($request);
+		SuperGlobal\HTTP_GET::G(SwooleSuperGlobalGet::G())->init($request);
+		SuperGlobal\HTTP_POST::G(SwooleSuperGlobalPost::G())->init($request);
+		SuperGlobal\HTTP_REQUEST::G(SwooleSuperGlobalRequest::G())->init($request);
+		SuperGlobal\COOKIE::G(SwooleSuperGlobalCookie::G())->init($request);
+		SuperGlobal\SERVER::Set('DOCUMENT_ROOT',rtrim($this->options['path'],'/'));
+		SuperGlobal\SERVER::Set('SCRIPT_FILENAME',$this->options['path'].'index.php');
 		
 		//reuse;
 		$route=DNRoute::G();
@@ -156,10 +195,12 @@ class SwooleAppBase extends DNMVCS
 	public function RunWithServer($server,$options)
 	{
 		DNSingletonStaticClass::$Replacer=[CoroutineSingleton::class,'G'];
-		SwooleHttpd::G()->bindHttp(
+		(new SwooleHttpd())->bindHttp(
 			$server,
 			function(){},
 			function($request,$response)use($options){
+
+
 				DN::G(DN::G()->init($options));
 				
 				DN::G()->options['request']=$request;
@@ -173,6 +214,7 @@ class SwooleAppBase extends DNMVCS
 				DN::G()->options['request']=null;
 				DN::G()->options['response']=null;
 				DN::G()->cleanUp();
+
 				CoroutineSingleton::CleanCoroutineSingleton();
 			}
 		);
