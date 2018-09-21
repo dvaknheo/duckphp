c1ddd556062edca1fa16e8d804a9679f1a5a22a5
dvakheo <dvaknheo@gmail.com>
2018-09-21 16:37:15 +0800

    chan 池子变更

--- DNMVCS/SwoolePool.php
+++ DNMVCS/SwoolePool.php
@@ -1,7 +1,11 @@
 <?php 
 namespace DNMVCS;
 class SwoolePool
-{
+{	
+	const MODE_AUTO=0;
+	const MODE_PROCESS=1;
+	const MODE_COROUTINE=2;
+	
 	protected static $_instances=[];
 	public static function G($object=null)
 	{
@@ -20,22 +24,27 @@ class SwoolePool
 	public $items;
 	public $max_length=10;
 	public $timeout=null;
-	public $create_handler=null;
-
-	public function init($create_handler,$max_length=10,$timeout=null,$no_co=false)
+	public function init($max_length=10,$timeout=null,$mode=self::MODE_AUTO)
 	{
 		$this->max_length=$max_length;
-		$this->create_handler=$create_handler;
 		$this->timeout=$timeout;
-		$cid = \Swoole\Coroutine::getuid();
-		if($no_co || $cid<=0){
-			$this->items = new \Swoole\Channel(1);
-		}else{
+		
+		if($mode===self::MODE_COROUTINE){
 			$this->items = new \Swoole\Coroutine\Channel(1);
+		}else if($mode===self::MODE_PROCESS){
+			$this->items = new \Swoole\Channel(1);
+		}else if($mode===self::MODE_AUTO){
+			$cid = \Swoole\Coroutine::getuid();
+			if($cid<=0){
+				$this->items = new \Swoole\Channel(1);
+			}else{
+				$this->items = new \Swoole\Coroutine\Channel(1);
+			}
+		}else{
+			throw new Exception('Bad Mode');
 		}
 	}
-	
-	public function getObject()
+	public function getObject($args)
 	{
 		do{
 			if($this->items->stats()['queue_num']==0){
@@ -47,25 +56,32 @@ class SwoolePool
 				$now=time();
 				$is_timeout =($now-$time)>$this->timeout?true:false;
 				if($is_timeout){
+					$this->gcObject($object);
 					break;
 				}
 			}
 			return $object;
 		}while(true);
 		
-		$object=$this->createObject();
+		$object=$this->createObject($args);
 		return $object;
 	}
-	protected function createObject()
+	protected function createObject($args)
 	{
-		$object=($this->create_handler)();
+		throw new Exception('Impelement Me');
+		$object=null;
 		return $object;
+	}
+	protected function gcObject($object)
+	{
+		//for override;
 	}
 	public function releaseObject($object)
 	{
 		if($this->items->stats()['queue_num']>=$this->max_length){
 			return;
 		}
+		//debug_print_backtrace();exit;
 		$flag=$this->items->push([$object,time()]);
 	}
 }
