31722d4acc6ed4de56adceb0dfc5d3f499c6654e
dvakheo <dvaknheo@gmail.com>
2019-02-01 13:48:55 +0800

    DBProxy 移到 DNMVCSExt

--- DNMVCSExt.php
+++ DNMVCSExt.php
@@ -324,6 +324,91 @@ class RouteHookDirectoryMode // not working.
 	}
 }
 
+
+class DBConnectPoolProxy
+{
+	use DNSingleton;
+	
+	public $tag_write='0';
+	public $tag_read='1';
+	
+	protected $db_create_handler;
+	protected $db_close_handler;
+	protected $db_queue_write;
+	protected $db_queue_write_time;
+	protected $db_queue_read;
+	protected $db_queue_read_time;
+	public $max_length=100;
+	public $timeout=5;
+	public function __construct()
+	{
+		$this->db_queue_write=new \SplQueue();
+		$this->db_queue_write_time=new \SplQueue();
+		$this->db_queue_read=new \SplQueue();
+		$this->db_queue_read_time=new \SplQueue();
+	}
+	public function init($max_length=10,$timeout=5,$dbm=null)
+	{
+		$this->max_length=$max_length;
+		$this->timeout=$timeout;
+		$this->proxy($dbm);
+		return $this;
+	}
+	public function setDBHandler($db_create_handler,$db_close_handler=null)
+	{
+		$this->db_create_handler=$db_create_handler;
+		$this->db_close_handler=$db_close_handler;
+	}
+	protected function getObject($queue,$queue_time,$db_config,$tag)
+	{
+		if($queue->isEmpty()){
+			return ($this->db_create_handler)($db_config,$tag);
+		}
+		$db=$queue->shift();
+		$time=$queue_time->shift();
+		$now=time();
+		$is_timeout =($now-$time)>$this->timeout?true:false;
+		if($is_timeout){
+			($this->db_close_handler)($db,$tag);
+			return ($this->db_create_handler)($db_config,$tag);
+		}
+		return $db;
+		
+	}
+	protected function reuseObject($queue,$queue_time,$db)
+	{
+		if(count($queue)>=$this->max_length){
+			($this->db_close_handler)($db,$tag);
+			return;
+		}
+		$time=time();
+		$queue->push($db);
+		$queue_time->push($time);
+	}
+	public function onCreate($db_config,$tag)
+	{
+		if($tag!=$this->tag_write){
+			return $this->getObject($this->db_queue_write,$this->db_queue_write_time,$db_config,$tag);
+		}else{
+			return $this->getObject($this->db_queue_read,$this->db_queue_read_time,$db_config,$tag);
+		}
+	}
+	public function onClose($db,$tag)
+	{
+		if($tag!=$this->tag_write){
+			return $this->reuseObject($this->db_queue_write,$this->db_queue_write_time,$db);
+		}else{
+			return $this->reuseObject($this->db_queue_read,$this->db_queue_read_time,$db);
+		}
+	}
+	public function proxy($dbm)
+	{
+		if(!$dbm){ return; }
+		
+		$this->setDBHandler($dbm->db_create_handler,$dbm->db_close_handler);
+		$dnm->setDBHandler([$this,'onCreate'],[$this,'onClose']);
+	}
+}
 class ProjectCommonAutoloader
 {
 	use DNSingleton;
@@ -568,6 +653,9 @@ class DNMVCSExt
 			'dir_mode_use_path_info'=>true,
 			'mode_dir_key_for_module'=>true,
 			'mode_dir_key_for_action'=>true,
+			
+			'db_reuse_size'=>0,
+			'db_reuse_timeout'=>5,
 		];
 	protected $has_enableFacade=false;
 	public function afterInit($dn)
@@ -613,6 +701,11 @@ class DNMVCSExt
 			$this->enableFacade();
 		}
 		
+		$db_reuse_size=$options['db_reuse_size']??static::DEFAULT_DN_OPTIONS['db_reuse_size'];
+		$db_reuse_timeout=$options['db_reuse_timeout']??static::DEFAULT_DN_OPTIONS['db_reuse_timeout'];
+		if($db_reuse_size){
+			DBConnectPoolProxy::G()->init($db_reuse_size,$db_reuse_timeout,DNDBManager::G());
+		}
 		
 	}
 	protected function enableFacade()
