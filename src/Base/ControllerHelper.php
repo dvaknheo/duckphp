44559dc6ccfdc3f08acf15d47a0744ea25a8c382
dvakheo <dvaknheo@gmail.com>
2019-04-30 17:26:58 +0800

    又去掉了 glue...

--- src/Base/ControllerHelper.php
+++ src/Base/ControllerHelper.php
@@ -2,11 +2,58 @@
 namespace DNMVCS\Base;
 
 use DNMVCS\Core\Base\ControllerHelper as Helper;
-use DNMVCS\Glue\GlueSuperGlobal;
-use DNMVCS\Glue\GlueForController;
+
+use DNMVCS\Core\App;
+use DNMVCS\Basic\SuperGlobal;
 
 class ControllerHelper extends Helper
 {
-    use GlueSuperGlobal;
-    use GlueForController;
+    ///////
+    public static function Import($file)
+    {
+        return App::G()->_Import($file);
+    }
+    public static function RecordsetUrl(&$data, $cols_map=[])
+    {
+        return App::G()->_RecordsetUrl($data, $cols_map);
+    }
+    
+    public static function RecordsetH(&$data, $cols=[])
+    {
+        return App::G()->_RecordsetH($data, $cols);
+    }
+    ///////
+    public static function SG()
+    {
+        return SuperGlobal::G();
+    }
+    public static function &GLOBALS($k, $v=null)
+    {
+        return SuperGlobal::G()->_GLOBALS($k, $v);
+    }
+    
+    public static function &STATICS($k, $v=null)
+    {
+        return SuperGlobal::G()->_STATICS($k, $v, 1);
+    }
+    public static function &CLASS_STATICS($class_name, $var_name)
+    {
+        return SuperGlobal::G()->_CLASS_STATICS($class_name, $var_name);
+    }
+    public static function session_start(array $options=[])
+    {
+        return SuperGlobal::G()->session_start($options);
+    }
+    public function session_id($session_id=null)
+    {
+        return SuperGlobal::G()->session_id($session_id);
+    }
+    public static function session_destroy()
+    {
+        return SuperGlobal::G()->session_destroy();
+    }
+    public static function session_set_save_handler(\SessionHandlerInterface $handler)
+    {
+        return SuperGlobal::G()->session_set_save_handler($handler);
+    }
 }
--- src/Base/ModelHelper.php
+++ src/Base/ModelHelper.php
@@ -2,9 +2,20 @@
 namespace DNMVCS\Base;
 
 use DNMVCS\Core\Base\ModelHelper as Helper;
-use DNMVCS\Glue\GlueDBManager;
+use DNMVCS\InnerExt\DBManager;
 
 class ModelHelper extends Helper
 {
-    use GlueDBManager;
+    public static function DB($tag=null)
+    {
+        return DBManager::G()->_DB($tag);
+    }
+    public static function DB_W()
+    {
+        return DBManager::G()->_DB_W();
+    }
+    public static function DB_R()
+    {
+        return DBManager::G()->_DB_R();
+    }
 }
