d2d0432409ebdf15eafe144d8809ee4e1e8852d6
dvakheo <dvaknheo@gmail.com>
2019-05-01 11:17:50 +0800

    trait ThrowQuickly 移动到 Toolkit 里

--- src/Basic/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace DNMVCS\Basic;
-
-use DNMVCS\Basic\ThrowQuickly;
-
-class Exception extends \Exception
-{
-    use ThrowQuickly;
-}
--- src/Toolkit/ToolKit.php
+++ src/Toolkit/ToolKit.php
@@ -392,3 +392,15 @@ class Ticker
         }
     }
 }
+trait ThrowQuickly
+{
+    public static function ThrowOn($flag, $message, $code=0)
+    {
+        if (!$flag) {
+            return;
+        }
+        $class=static::class;
+        throw new $class($message, $code);
+    }
+}
+
