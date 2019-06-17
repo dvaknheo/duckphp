710de22b4a22c8a68f945f18ea35a9f4a513f545
dvakheo <dvaknheo@gmail.com>
2019-06-17 12:26:51 +0800

    ToolKit 去掉重复函数

--- src/Toolkit/ToolKit.php
+++ src/Toolkit/ToolKit.php
@@ -192,49 +192,6 @@ class DNFuncionModifer
     }
 }
 
-class API
-{
-    protected static function GetTypeFilter()
-    {
-        return [
-            'boolean'=>FILTER_VALIDATE_BOOLEAN  ,
-            'bool'=>FILTER_VALIDATE_BOOLEAN  ,
-            'int'=>FILTER_VALIDATE_INT,
-            'float'=>FILTER_VALIDATE_FLOAT,
-            'string'=>FILTER_SANITIZE_STRING,
-        ];
-    }
-    public static function Call($class, $method, $input)
-    {
-        $f=self::GetTypeFilter();
-        $reflect = new ReflectionMethod($class, $method);
-        
-        $params=$reflect->getParameters();
-        $args=array();
-        foreach ($params as $i => $param) {
-            $name=$param->getName();
-            if (isset($input[$name])) {
-                $type=$param->getType();
-                if (null!==$type) {
-                    $type=''.$type;
-                    if (in_array($type, array_keys($f))) {
-                        $flag=filter_var($input[$name], $f[$type], FILTER_NULL_ON_FAILURE);
-                        DNMVCS::ThrowOn($flag===null, "Type Unmatch: {$name}", -1);
-                    }
-                }
-                $args[]=$input[$name];
-                continue;
-            } elseif ($param->isDefaultValueAvailable()) {
-                $args[]=$param->getDefaultValue();
-            } else {
-                DNMVCS::ThrowOn(true, "Need Parameter: {$name}", -2);
-            }
-        }
-        
-        $ret=$reflect->invokeArgs(new $service(), $args);
-        return $ret;
-    }
-}
 class MyArgsAssoc
 {
     protected static function GetCalledAssocByTrace($trace)
