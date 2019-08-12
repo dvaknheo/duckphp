5b151fcbf2a947d94b280177f3a416419e0711a3
dvakheo <dvaknheo@gmail.com>
2019-08-12 16:07:41 +0800

    继续调DB连接池

--- src/Ext/DBReusePoolProxy.php
+++ src/Ext/DBReusePoolProxy.php
@@ -13,37 +13,23 @@ class DBReusePoolProxy
         'db_reuse_timeout' => 5,
         'dbm' => null,
     ];
-    public $tag_write='0';
-    public $tag_read='1';
     public $max_length=100;
     public $timeout=5;
     
     protected $db_create_handler;
     protected $db_close_handler;
-    protected $db_queue_write;
-    protected $db_queue_write_time;
-    protected $db_queue_read;
-    protected $db_queue_read_time;
     
+    protected $pools=[];
     protected $appClass;
     
-    protected $dn;
-    
-    public function __construct()
-    {
-        $this->db_queue_write=new SplQueue();
-        $this->db_queue_write_time=new SplQueue();
-        $this->db_queue_read=new SplQueue();
-        $this->db_queue_read_time=new SplQueue();
-    }
     public function init($options=[], $context=null)
     {
         $options=array_replace_recursive(static::DEFAULT_OPTIONS, $options);
         
         $this->max_length=$options['db_reuse_size'];
         $this->timeout=$options['db_reuse_timeout'];
-        $dbm=$options['dbm'];
         
+        $dbm=$options['dbm'];
         $dbm=$dbm??DBManager::G();
         $dbm=is_string($dbm)?$dbm::G():$dbm;
         
@@ -57,13 +43,17 @@ class DBReusePoolProxy
         $this->db_create_handler=$db_create_handler;
         $this->db_close_handler=$db_close_handler;
     }
-    protected function getObject($queue, $queue_time, $db_config, $tag)
+    protected function getObject($db_config, $tag)
     {
-        if ($queue->isEmpty()) {
+        if(!isset($this->pools[$tag])){
+            $this->pools[$tag]=[];
+        }
+        if (empty($this->pools[$tag])) {
             return ($this->db_create_handler)($db_config, $tag);
         }
-        $db=$queue->shift();
-        $time=$queue_time->shift();
+        $data=array_shift($this->pools[$tag]);
+        list($db,$time,$enabled)=$data;
+        
         $now=time();
         $is_timeout =($now-$time)>$this->timeout?true:false;
         if ($is_timeout) {
@@ -72,29 +62,30 @@ class DBReusePoolProxy
         }
         return $db;
     }
-    protected function reuseObject($queue, $queue_time, $db)
+    protected function reuseObject($db)
     {
-        if (count($queue)>=$this->max_length) {
+        if(!isset($this->pools[$tag])){
+            $this->pools[$tag]=[];
+        }
+        if (count($this->pools[$tag])>=$this->max_length) {
             ($this->db_close_handler)($db, $tag);
             return;
         }
         $time=time();
-        $queue->push($db);
-        $queue_time->push($time);
+        $data=array($db,$time,true);
+        
+        array_push($this->pools,$data);
     }
     public function onCreate($db_config, $tag)
     {
-        if ($tag!=$this->tag_write) {
-            return $this->getObject($this->db_queue_write, $this->db_queue_write_time, $db_config, $tag);
-        } else {
-            return $this->getObject($this->db_queue_read, $this->db_queue_read_time, $db_config, $tag);
-        }
+        return $this->getObject($db_config, $tag);
     }
     protected function checkException()
     {
         if (!$this->appClass) {
             return false;
         }
+        // TODO;
         return ($this->appClass)::G()->isInException();
     }
     public function onClose($db, $tag)
@@ -102,19 +93,15 @@ class DBReusePoolProxy
         if ($this->checkException()) {
             return;
         }
-        if ($tag!=$this->tag_write) {
-            return $this->reuseObject($this->db_queue_write, $this->db_queue_write_time, $db);
-        } else {
-            return $this->reuseObject($this->db_queue_read, $this->db_queue_read_time, $db);
-        }
+        return $this->reuseObject($db, $tag);
     }
     public function proxy($dbm)
     {
         if (!$dbm) {
             return;
         }
-        list($db_create_handler, $db_close_handler)=$dnm->getDBHandler();
+        list($db_create_handler, $db_close_handler)=$dbm->getDBHandler();
         $this->setDBHandler($db_create_handler, $db_close_handler);
-        $dnm->setDBHandler([$this,'onCreate'], [$this,'onClose']);
+        $dbm->setDBHandler([$this,'onCreate'], [$this,'onClose']);
     }
 }
