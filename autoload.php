169915ae7633d18d2309d7ee66eb77e9ee08e4b9
dvaknheo <dvaknheo@gmail.com>
2020-05-14 11:07:18 +0800

    优化外部 autoload 函数

--- autoload.php
+++ autoload.php
@@ -6,9 +6,6 @@ function _dnmvcs_namespace_autoload($class) {
         return false;
     }
     $file = $path . str_replace('\\', '/', substr($class, strlen($namespace))) . '.php';
-    if (!file_exists($file)) {
-        return false;
-    }
     require_once $file;
     return true;
 }
