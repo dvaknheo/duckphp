e81fdedb738e68034402df86497bd4038121e1f1
dvaknheo <dvaknheo@gmail.com>
2020-06-17 23:58:25 +0800

    添加 phpdoc

--- src/Core/SingletonEx.php
+++ src/Core/SingletonEx.php
@@ -9,6 +9,11 @@ namespace DuckPhp\Core;
 trait SingletonEx
 {
     protected static $_instances = [];
+    /**
+     * 
+     * @param type $object
+     * @return \static
+     */
     public static function G($object = null)
     {
         if (defined('__SINGLETONEX_REPALACER')) {
