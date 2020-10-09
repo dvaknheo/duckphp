9082db6724aa87fabed4db32238a5cd681a11faa
dvaknheo <dvaknheo@gmail.com>
2020-10-09 20:44:18 +0800

    完成类名变更

--- tests/Ext/RouteHookPathInfoCompatTest.php
+++ tests/Ext/RouteHookPathInfoCompatTest.php
@@ -1,46 +1,46 @@
 <?php
 namespace tests\DuckPhp\Ext;
 
-use DuckPhp\Ext\RouteHookPathInfoByGet;
+use DuckPhp\Ext\RouteHookPathInfoCompat;
 use DuckPhp\Core\App;
 use DuckPhp\Core\Route;
 use DuckPhp\Core\SuperGlobal;
 
-class RouteHookPathInfoByGetTest extends \PHPUnit\Framework\TestCase
+class RouteHookPathInfoCompatTest extends \PHPUnit\Framework\TestCase
 {
     public function testAll()
     {
-        \MyCodeCoverage::G()->begin(RouteHookPathInfoByGet::class);
+        \MyCodeCoverage::G()->begin(RouteHookPathInfoCompat::class);
         
         $route_options=[
             'namespace'=>__NAMESPACE__,
             'namespace_controller'=>'\\'.__NAMESPACE__,
-            'controller_welcome_class'=> 'RouteHookPathInfoByGetTestMain',
+            'controller_welcome_class'=> 'RouteHookPathInfoCompatTestMain',
 
         ];
         Route::G(new Route())->init($route_options);
         App::G()->init(['skip_setting_file'=>true]);
         $options=[
-            'use_path_info_by_get'=>false,
-            'key_for_action'=>'',
-            'key_for_module'=>'',
+            'path_info_compact_enable'=>false,
+            'path_info_compact_action_key'=>'',
+            'path_info_compact_class_key'=>'',
 
         ];
-        RouteHookPathInfoByGet::G(new RouteHookPathInfoByGet())->init($options, App::G());
+        RouteHookPathInfoCompat::G(new RouteHookPathInfoCompat())->init($options, App::G());
         
         $options=[
-            'use_path_info_by_get'=>true,
-            'key_for_action'=>'',
-            'key_for_module'=>'',
+            'path_info_compact_enable'=>true,
+            'path_info_compact_action_key'=>'',
+            'path_info_compact_class_key'=>'',
         ];
-        RouteHookPathInfoByGet::G(new RouteHookPathInfoByGet())->init($options);
+        RouteHookPathInfoCompat::G(new RouteHookPathInfoCompat())->init($options);
         $options=[
-            'use_path_info_by_get'=>true,
+            'path_info_compact_enable'=>true,
 
-            'key_for_action'=>'_r',
-            'key_for_module'=>'',
+            'path_info_compact_action_key'=>'_r',
+            'path_info_compact_class_key'=>'',
         ];
-        RouteHookPathInfoByGet::G()->init($options, App::G());
+        RouteHookPathInfoCompat::G()->init($options, App::G());
         
         
 
@@ -56,7 +56,7 @@ class RouteHookPathInfoByGetTest extends \PHPUnit\Framework\TestCase
         
         echo "------------------------------------------------\n";
 if(true){
-        RouteHookPathInfoByGet::G()->onURL("zzz");
+        RouteHookPathInfoCompat::G()->onURL("zzz");
         echo "------------------------------------------------\n";
 }
         //x/index.php/init
@@ -64,32 +64,32 @@ if(true){
         SuperGlobal::G()->_SERVER['PATH_INFO']='/model/action';
         SuperGlobal::G()->_SERVER['SCRIPT_FILENAME']='/test/index.php';
         $options=[
-            'key_for_action'=>'_r',
-            'key_for_module'=>'m',
+            'path_info_compact_action_key'=>'_r',
+            'path_info_compact_class_key'=>'m',
         ];
         var_dump(Route::URL('/Test'));
 
-        RouteHookPathInfoByGet::G()->init($options);
+        RouteHookPathInfoCompat::G()->init($options);
         
         var_dump(Route::URL(''));
 
-        var_dump(RouteHookPathInfoByGet::URL('index.php/bb?cc=dd&m=abc'));
-        var_dump(RouteHookPathInfoByGet::URL('aa/bb?cc=dd&m=abc'));
-        var_dump(RouteHookPathInfoByGet::URL('aa/bb?cc=dd&m='));
+        var_dump(RouteHookPathInfoCompat::URL('index.php/bb?cc=dd&m=abc'));
+        var_dump(RouteHookPathInfoCompat::URL('aa/bb?cc=dd&m=abc'));
+        var_dump(RouteHookPathInfoCompat::URL('aa/bb?cc=dd&m='));
 
         //------------
         
-                        RouteHookPathInfoByGet::G()->isInited();
+                        RouteHookPathInfoCompat::G()->isInited();
 
         \MyCodeCoverage::G()->end();
         /*
-        RouteHookPathInfoByGet::G()->init($options=[], $context=null);
-        RouteHookPathInfoByGet::G()->onURL($url=null);
-        RouteHookPathInfoByGet::G()->hook($route);
+        RouteHookPathInfoCompat::G()->init($options=[], $context=null);
+        RouteHookPathInfoCompat::G()->onURL($url=null);
+        RouteHookPathInfoCompat::G()->hook($route);
         //*/
     }
 }
-class RouteHookPathInfoByGetTestMain
+class RouteHookPathInfoCompatTestMain
 {    
     function index(){
         var_dump(DATE(DATE_ATOM));
