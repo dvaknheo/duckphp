2d6a8c4ed4ad91e1df56ea8abcd706dc63fae2f7
dvaknheo <dvaknheo@gmail.com>
2020-11-18 08:44:30 +0800

    修复默认设置

--- template/public/full/SimpleBlog/config/setting.php
+++ template/public/full/SimpleBlog/config/setting.php
@@ -4,4 +4,15 @@
  * From this time, you never be alone~
  */
 
-return require __DIR__.'/../../CommonConfig/setting.php';
+return [
+    'duckphp_is_debug' => true,
+    'duckphp_platform' => 'default',
+    'database_list' => [
+        [
+        'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=DnSample;charset=utf8mb4;',
+        'username' => 'admin',
+        'password' => '123456',
+        'driver_options' => [],
+        ],
+    ],
+];
