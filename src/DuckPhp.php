05b8bdaf68110a4d8b6257813f39692aa9bfa786
dvaknheo <85811616@qq.com>
2023-09-07 11:35:55 +0800

    完成最后一个功能：便于安装

--- src/DuckPhp.php
+++ src/DuckPhp.php
@@ -47,10 +47,66 @@ class DuckPhp extends App
                 RedisManager::class
                 ]);
         }
-        //我们要加个可以InstallableTrait;
-        
+        if ($this->options['ext_options_from_config']??false) {
+            $this->mergeExtOptions();
+        }
         return $this;
     }
+    public function getPath($sub_path)
+    {
+        $key = "path_".$sub_path;
+        if(isset($this->options[$key])){
+            return parent::getComponentPathByKey($key);
+        }
+        return $this->options['path'].$sub_path .'/';
+    }
+    //////////////////////
+    protected $file_for_ext_options_from_config = 'DuckPhpOptions';
+    public function isInstalled()
+    {
+       return $this->options['install']? true : false;
+    }
+    public function install($options)
+    {
+        $options['install'] = DATE(DATE_ATOM);
+        $this->saveExtOptions(static::class, $options);
+    }
+    
+    protected function get_file_for_ext_config()
+    {
+        $self = $this;
+        $path = $this->_PhaseCall(get_class(App::G()),function()use($self){ return App::G()->getPath('config');});
+        $full_file = $path.$this->file_for_ext_options_from_config .'.php';
+        return $full_file;
+    }
+    protected function get_all_ext_config($full_file = null)
+    {
+        clearstatcache();
+        $full_file = $full_file ?? $this->get_file_for_ext_config();
+        if(!is_file($full_file)){
+            return [];
+        }
+        $all_options =  include($full_file); //TODO seprate
+        return $all_options;
+    }
+    protected function mergeExtOptions()
+    {
+        $all_options = $this->get_all_ext_config();
+        $options = $all_options[static::class]??[];
+        $this->options = array_replace_recursive($this->options, $options);
+    }
+    protected function saveExtOptions($class, $options)
+    {
+        $full_file = $this->get_file_for_ext_config();
+        $all_options = $this->get_all_ext_config($full_file);
+        
+        $all_options[$class] = $options;
+        
+        $string ="<"."?php //". "regenerate by " . __CLASS__ . '->'.__METHOD__ ." at ". DATE(DATE_ATOM) . "\n";        
+        $string .="return ".var_export($all_options, true) .';';
+        file_put_contents($full_file,$string);
+    }
+    /////////////////
     public static function Admin($admin = null)
     {
         return static::G()->_Admin($admin);
