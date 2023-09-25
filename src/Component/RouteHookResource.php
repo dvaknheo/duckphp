07b91552a51daab90bc5b66b3e39950a35fc29f2
dvaknheo <85811616@qq.com>
2023-09-25 09:54:50 +0800

    给 RouteHookResource 添加 path_override_from 选项

--- src/Component/RouteHookResource.php
+++ src/Component/RouteHookResource.php
@@ -11,8 +11,9 @@ class RouteHookResource extends ComponentBase
 {
     public $options = [
         'path' => '',
+        'path_override_from' => '',
         'path_resource' => 'res',
-        //TODO 'controller_url_prefix'
+        'controller_url_prefix' => '',
         'controller_resource_prefix' => '',
     ];
     public static function Hook($path_info)
@@ -27,10 +28,13 @@ class RouteHookResource extends ComponentBase
     public function _Hook($path_info)
     {
         $file = urldecode(''.$path_info);
-        $prefix = $this->options['controller_resource_prefix'];
+        $prefix = $this->options['controller_url_prefix'];
+        $prefix = $prefix?'/'.$prefix:'';
+        $prefix .= $this->options['controller_resource_prefix'];
         if (!empty($prefix) && (substr($file, 0, strlen($prefix)) !== $prefix)) {
             return false;
         }
+        
         if (!empty($prefix)) {
             $file = substr($file, strlen($prefix));
         }
@@ -40,12 +44,17 @@ class RouteHookResource extends ComponentBase
         if (pathinfo($file, PATHINFO_EXTENSION) === 'php') {
             return false;
         }
-        $file = ComponentBase::GetFileFromSubComponent($this->options, 'resource', $file);
+        
+        $options = $this->options;
+        $options['path']=$options['path_override_from']?$options['path_override_from']:$options['path'];
+        
+        $file = ComponentBase::GetFileFromSubComponent($options, 'resource', $file);
         if (!$file) {
             return false;
         }
         ($this->context_class)::header('Content-Type: '.($this->context_class)::mime_content_type($file));
         echo file_get_contents($file);
+        ($this->context_class)::exit();
         return true;
     }
 }
