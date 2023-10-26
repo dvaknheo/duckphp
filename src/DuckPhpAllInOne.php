3693015ab0d10d7e096f255b195f0098e2066793
dvaknheo <85811616@qq.com>
2023-10-26 19:43:38 +0800

    添加 DuckPhpAllInOne 骨架

---
+++ src/DuckPhpAllInOne.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPhp
+ * From this time, you never be alone~
+ */
+//dvaknheo@github.com
+//OK，Lazy
+
+namespace DuckPhp;
+
+
+
+class DuckPhpAllInOne extends DuckPhp
+{
+
+}
---
+++ tests/DuckPhpAllInOneTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace tests\DuckPhp;
+
+use DuckPhp\DuckPhpAllInOne;
+
+class DuckPhpAllInOneTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAll()
+    {
+        \LibCoverage\LibCoverage::Begin(DuckPhpAllInOne::class);
+        $LibCoverage = \LibCoverage\LibCoverage::G();
+        
+        \LibCoverage\LibCoverage::G($LibCoverage);
+        \LibCoverage\LibCoverage::End(DuckPhpAllInOne::class);
+
+    }
+
+}
