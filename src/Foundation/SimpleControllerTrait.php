0555707568fbeb7dac48fd63b2ca25d2910d6585
dvaknheo <85811616@qq.com>
2023-10-31 12:50:20 +0800

    三联通过，SimpleControllerTrait 重做，SimpleActionTrait 删除

--- src/Foundation/SimpleControllerTrait.php
+++ src/Foundation/SimpleControllerTrait.php
@@ -6,21 +6,82 @@
 namespace DuckPhp\Foundation;
 
 use DuckPhp\Component\CallInPhaseTrait;
+use DuckPhp\Core\PhaseContainer;
 use DuckPhp\Core\Route;
 
 trait SimpleControllerTrait
 {
     use CallInPhaseTrait;
-
+    
     public static function _($object = null)
     {
-        if ($object) {
-            Route::_()->replaceController(static::class, get_class($object));
-            return $object;
+        $route = Route::_();
+        $postfix = $route->options['controller_class_postfix'];
+        $class_base = $route->options['controller_class_base'];
+        
+        /*
+        postfix_set,postfix_match  base_set ,base_match , result
+        Y Y Y Y => Y
+        Y Y Y N => N
+        Y Y N * => Y
+        Y N * * => N
+        N * Y Y => Y
+        N * Y N => N
+        N * N * => Y
+        ------------
+        Y Y Y Y => Y
+        Y Y Y N => N
+        Y Y N Y => Y
+        Y Y N N => Y
+        Y N Y Y => N
+        Y N Y N => N
+        Y N N Y => N
+        Y N N N => N
+        N Y Y Y => Y
+        N N Y Y => Y
+        N Y Y N => N
+        N N Y N => N
+        N Y N Y => Y
+        N Y N N => Y
+        N N N Y => Y
+        N N N N => Y
+        //*/
+        $is_controller = false;
+        
+        if ($postfix) {
+            if (substr(static::class, -strlen($postfix)) === $postfix) {
+                if ($class_base) {
+                    if (\is_subclass_of(static::class, $class_base)) {
+                        $is_controller = true;
+                    } else {
+                        $is_controller = false;
+                    }
+                } else {
+                    $is_controller = true;
+                }
+            } else {
+                $is_controller = false;
+            }
         } else {
-            $class = Route::_()->options['controller_class_map'][static::class] ?? static::class;
+            if ($class_base) {
+                if (\is_subclass_of(static::class, $class_base)) {
+                    $is_controller = true;
+                } else {
+                    $is_controller = false;
+                }
+            } else {
+                $is_controller = true;
+            }
+        }
+        if ($is_controller) {
+            $class = $object ?  get_class($object) :static::class ;
+            if ($class !== static::class) {
+                $route->options['controller_class_map'][static::class] = $class;
+            }
             $object = (new \ReflectionClass($class))->newInstanceWithoutConstructor();
             return $object;
         }
+        $ret = PhaseContainer::GetObject(static::class, $object);
+        return $ret;
     }
 }
--- tests/Foundation/SimpleControllerTraitTest.php
+++ tests/Foundation/SimpleControllerTraitTest.php
@@ -1,6 +1,8 @@
 <?php 
 namespace tests\DuckPhp\Foundation;
 use DuckPhp\Foundation\SimpleControllerTrait;
+use DuckPhp\DuckPhpAllInOne;
+use DuckPhp\Core\Route;
 
 class SimpleControllerTraitTest extends \PHPUnit\Framework\TestCase
 {
@@ -9,17 +11,129 @@ class SimpleControllerTraitTest extends \PHPUnit\Framework\TestCase
         $LibCoverage = \LibCoverage\LibCoverage::G();
         \LibCoverage\LibCoverage::Begin(SimpleControllerTrait::class);
         
-        ControllerFakeSingletonTraitObject::_(ControllerFakeSingletonTraitObject2::_());
+        //ControllerFakeSingletonTraitObject::_(ControllerFakeSingletonTraitObject2::_());
+        $options = [
+            'namespace_controller' => __NAMESPACE__,
+            'controller_class_postfix' => 'Controller',
+            'controller_method_prefix' => '',
+            'controller_class_base' => '',
+        ];
+        Route::_()->init($options);
+        Route::_()->bind('/hello/world');
+        Route::_()->run();
+        helloController::_(helloController::_());
+        helloController::_(MyController::_());
+        Route::_()->run();
+        echo "-------------\n";
+        $options = [
+            'namespace_controller' => __NAMESPACE__,
+            'controller_class_postfix' => 'Controller',
+            'controller_method_prefix' => '',
+            'controller_class_base' => BaseX::class,
+        ];
+        Route::_(new Route())->init($options);
+        Route::_()->bind('/hello2/world');
+        hello2Controller::_(My2Controller::_());
+        Route::_()->run();
+        
+        echo "-------------\n";
+        $options = [
+            'namespace_controller' => __NAMESPACE__,
+            'controller_class_postfix' => '',
+            'controller_method_prefix' => '',
+            'controller_class_base' => BaseX::class,
+        ];
+        Route::_(new Route())->init($options);
+        Route::_()->bind('/hello2Controller/world');
+        hello2Controller::_(My2Controller::_());
+        Route::_()->run();
+        
+        echo "-------------\n";
+        $options = [
+            'namespace_controller' => __NAMESPACE__,
+            'controller_class_postfix' => '',
+            'controller_method_prefix' => '',
+            'controller_class_base' => '',
+        ];
+        Route::_(new Route())->init($options);
+        Route::_()->bind('/hello2Controller/world');
+        hello2Controller::_(My2Controller::_());
+        Route::_()->run();
+        
+        echo "-6666------------\n";
+        
+        
+        
+        
+        
+        
+        $options = [
+            'namespace_controller' => __NAMESPACE__,
+            'controller_class_postfix' => 'Controller',
+            'controller_method_prefix' => 'action',
+            'controller_class_base' => '',
+        ];
+        Route::_()->init($options);
+        Route::_()->bind('/hello/world');
+        helloController::_(helloController::_());
+        helloController::_(MyController::_());
+        Route::_()->run();
+        
+        MyAction::_(MyAction2::_())->foo();
         
         \LibCoverage\LibCoverage::G($LibCoverage);
         \LibCoverage\LibCoverage::End();
     }
 }
-class ControllerFakeSingletonTraitObject
+class helloController
+{
+    use SimpleControllerTrait;
+
+    public function __construct()
+    {
+        var_dump('new:' . static::class);
+    }
+    public function world()
+    {
+        var_dump('static::class: '.static::class);
+    }
+}
+
+class MyController extends helloController
+{
+}
+class BaseX
 {
     use SimpleControllerTrait;
 }
-class ControllerFakeSingletonTraitObject2 extends ControllerFakeSingletonTraitObject
+class hello2Controller extends BaseX
 {
     
+}
+class My2Controller extends hello2Controller
+{
+}
+class My3Controller
+{
+    use SimpleControllerTrait;
+
+}
+////////////////
+class MyAction extends BaseX
+{
+ public function __construct()
+    {
+        var_dump('MyAction:' . static::class);
+    }
+    public function foo()
+    {
+        var_dump('foo');
+    }
+}
+class MyAction2 extends MyAction
+{
+    public function foo()
+    {
+        var_dump('MyAction2MyAction2MyAction2');
+    }
 }
