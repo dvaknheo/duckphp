f66b2546fa153fcdf9eba46dfeb83524e6999262
dvaknheo <85811616@qq.com>
2023-11-06 22:34:17 +0800

    继续调整模板

--- template/public/advance/public/index.php
+++ template/public/advance/public/index.php
@@ -8,8 +8,10 @@ require_once(__DIR__.'/../../../../autoload.php');    //@DUCKPHP_HEADFILE
 echo "<div>You should not run the template file directly, Install it! </div>\n"; //@DUCKPHP_DELETE
 echo "<div>不建议直接运行模板文件，建议用安装模式 </div>\n";              //@DUCKPHP_DELETE
 
-\DuckPhp\Core\AutoLoader::RunQuickly([]);
-\DuckPhp\Core\AutoLoader::addPsr4( "AdvanceDemo\\", __DIR__ . '/../src',);
+if (!class_exists(\LazyToChange\System\App::class)) {
+    \DuckPhp\Core\AutoLoader::RunQuickly([]);
+    \DuckPhp\Core\AutoLoader::addPsr4( "AdvanceDemo\\", 'src');
+}
 
 $options = [
     //...
