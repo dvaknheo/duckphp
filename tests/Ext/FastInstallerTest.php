83c290a897fd2fd540363462641132b5b49f7486
dvaknheo <85811616@qq.com>
2024-01-27 20:37:10 +0800

    调整测试

---
+++ tests/Ext/FastInstallerTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace tests\DuckPhp\Ext;
+
+use DuckPhp\Ext\FastInstaller;
+
+class FastInstallerTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAll()
+    {
+        \LibCoverage\LibCoverage::Begin(FastInstaller::class);
+        //
+        \LibCoverage\LibCoverage::End();
+       
+    }
+}
---
+++ tests/Foundation/FastInstallerTraitTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace tests\DuckPhp\Foundation;
+
+use DuckPhp\Foundation\FastInstallerTrait;
+
+class FastInstallerTraitTest extends \PHPUnit\Framework\TestCase
+{
+    public function testAll()
+    {
+        \LibCoverage\LibCoverage::Begin(FastInstallerTrait::class);
+        //
+        \LibCoverage\LibCoverage::End();
+       
+    }
+}
