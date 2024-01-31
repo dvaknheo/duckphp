a1851e8c332bb0b47e8ba14f1f255b06e832aa08
dvaknheo <85811616@qq.com>
2024-01-31 22:40:34 +0800

    FastInstallerTraitTest 完成

--- tests/Foundation/FastInstallerTraitTest.php
+++ tests/Foundation/FastInstallerTraitTest.php
@@ -1,15 +1,32 @@
 <?php
 namespace tests\DuckPhp\Foundation;
 
+use DuckPhp\DuckPhp;
+use DuckPhp\Ext\FastInstaller;
 use DuckPhp\Foundation\FastInstallerTrait;
-
+class FiApp extends DuckPhp
+{
+    use FastInstallerTrait;
+}
+class MyFastInstaller extends FastInstaller
+{
+    public function doCommandInstall()
+    {
+        return;
+    }
+}
 class FastInstallerTraitTest extends \PHPUnit\Framework\TestCase
 {
     public function testAll()
     {
         \LibCoverage\LibCoverage::Begin(FastInstallerTrait::class);
-        //
+        $options = [];
+        FiApp::_()->init($options);
+        FastInstaller::_(MyFastInstaller::_());
+        FiApp::_()->command_require();
+        FiApp::_()->command_remove();
+        
+        FiApp::_()->command_install();
         \LibCoverage\LibCoverage::End();
-       
     }
 }
