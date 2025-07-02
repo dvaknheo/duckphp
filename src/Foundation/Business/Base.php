c2c33a18082946cdf259ff81d2eb3c683f34c439
dvaknheo <85811616@qq.com>
2025-07-02 14:13:04 +0800

    添加Base文件

---
+++ src/Foundation/Business/Base.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPhp
+ * From this time, you never be alone~
+ */
+namespace DuckPhp\Foundation\Business;
+
+use DuckPhp\Foundation\SimpleBusinessTrait;
+
+class Base
+{
+    use SimpleBusinessTrait;
+}
---
+++ src/Foundation/Model/Base.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+/**
+ * DuckPhp
+ * From this time, you never be alone~
+ */
+namespace DuckPhp\Foundation\Model;
+
+use DuckPhp\Foundation\SimpleModelTrait;
+
+class Base
+{
+    use SimpleModelTrait;
+}
