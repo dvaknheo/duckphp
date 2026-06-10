1708558465ee4180fe3fe66718ee264225db7c06
dvaknheo <85811616@qq.com>
2026-06-10 15:22:36 +0800

    完成测试文件和普通文件隔离

--- docker/test-php74/test-docker.sh
+++ docker/test-php74/test-docker.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# 在 Docker (PHP 7.4) 环境下跑测试
-
 cd "$(dirname "$0")"
-
-# 安装依赖的公共逻辑
-INSTALL_CMD="cp docker/test-php74/composer.lock.docker composer.lock && ([ -f /DATA/vendor/autoload.php ] || composer install --no-interaction --prefer-dist)"
-
-# 如果没有传参数，跑 fulltest（使用 docker-compose.yml 里默认的 command）
+INSTALL_CMD="[ -f /DATA/vendor/autoload.php ] || composer install --no-interaction --prefer-dist"
 if [ $# -eq 0 ]; then
     docker-compose run --rm fulltest
     exit $?
 fi
-
-# 有参数时，先确保依赖已安装，再执行传入的命令
 docker-compose run --rm fulltest sh -c "$INSTALL_CMD && exec \$@" sh "$@"
