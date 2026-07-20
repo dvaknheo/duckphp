9047fc882696feef6742cc4cb364f6e9578ecc29
dvaknheo <85811616@qq.com>
2026-07-20 14:58:35 +0800

    KernelTrait.php  的 cli_enable false 的时候强制走 route

--- src/Core/KernelTrait.php
+++ src/Core/KernelTrait.php
@@ -259,7 +259,7 @@ trait KernelTrait
         
 
         $this->is_root = is_null($context) || !(\is_a($context, self::class) || (static::class === self::class));
-        $this->is_cli = PHP_SAPI === 'cli' || $this->options['cli_enable'];
+        $this->is_cli = PHP_SAPI === 'cli' && $this->options['cli_enable'];
         $this->initContainer($context);
         $this->onPrepare();
         
