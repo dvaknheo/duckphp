653b8d51938c048ed7f83bc269e6e7d9d6d25280
dvaknheo <85811616@qq.com>
2026-07-21 10:24:02 +0800

    调整php84的docker

--- docker/test-php84/start-docker.sh
+++ docker/test-php84/start-docker.sh
@@ -6,7 +6,7 @@ if docker ps -a --format '{{.Names}}' | grep -q '^duckphp-test84$'; then
     exit 0
 fi
 
-docker-compose run -d --name duckphp-test84 --use-aliases fulltest tail -f /dev/null
+docker-compose run -d --name duckphp-test84 --use-aliases fulltest84 tail -f /dev/null
 
 # 启动 redis，后续 exec 不需要再启动
 docker exec duckphp-test84 redis-server --requirepass 123456 --daemonize yes
