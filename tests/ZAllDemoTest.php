6e0a713130eb6027bd9918e91e8ee90f28100c4f
dvaknheo <85811616@qq.com>
2026-08-06 20:40:21 +0800

    调整 ZAllDemoTest

--- tests/ZAllDemoTest.php
+++ tests/ZAllDemoTest.php
@@ -24,6 +24,10 @@ class ZAllDemoTest extends \PHPUnit\Framework\TestCase
         foreach($tests as $k => $len){
             $data = $this->curl_file_get_contents($host.$k);
             $data =str_replace(realpath(__DIR__.'/../'),'',$data);
+            if($k === 'files'){
+                // 裁剪动态内容（执行时间/内存、调用堆栈、包含文件），使 php74/php84 输出一致
+                $data = $this->normalizeFilesContent($data);
+            }
             
             $l=strlen($data);
             if($l!==$len){
@@ -51,5 +55,36 @@ class ZAllDemoTest extends \PHPUnit\Framework\TestCase
         $data = curl_exec($ch);
         curl_close($ch);
         return $data !== false?$data:'';
+    }
+    /**
+     * 裁剪 files 页面中随运行/版本变化的 fieldset 块（执行时间/内存、调用堆栈、包含文件）。
+     * @param string $data
+     * @return string
+     */
+    protected function normalizeFilesContent($data)
+    {
+        $lines = explode("\n", $data);
+        $ret = [];
+        $skip = false;
+        foreach ($lines as $line) {
+            if (strpos($line, '<fieldset>') !== false) {
+                $skip = false;
+            }
+            if (!$skip && strpos($line, '<legend>') !== false && (
+                strpos($line, '执行时间') !== false ||
+                strpos($line, '调用堆栈') !== false ||
+                strpos($line, '包含文件') !== false
+            )) {
+                $skip = true;
+            }
+            if ($skip) {
+                if (strpos($line, '</fieldset>') !== false) {
+                    $skip = false;
+                }
+                continue;
+            }
+            $ret[] = $line;
+        }
+        return implode("\n", $ret);
     }
 }
--- tests/data_for_tests/ZAllDemoTest.config.php
+++ tests/data_for_tests/ZAllDemoTest.config.php
@@ -14,7 +14,7 @@ return [
         'test/done'          => 95,
         'doc.php'            => 1329,
         ''                   => 1363,
-        'files'              => 11848,
+        'files'              => 7868,
         'demo.php'           => 406,
         'helloworld.php'     => 11,
         'just-route.php'     => 109,
