0672ba586515776782d0f7b4f79744341fd65ea2
dvaknheo <85811616@qq.com>
2026-08-08 09:13:46 +0800

    测试文件入库

---
+++ tests/data_for_tests/Component/RouteLister/Controller/AdminController.php
@@ -0,0 +1,10 @@
+<?php
+namespace tests_Ext_RouteLister\Controller;
+use DuckPhp\GlobalAdmin\AdminControllerInterface;
+
+class AdminController implements AdminControllerInterface
+{
+    public function action_admin()
+    {
+    }
+}
---
+++ tests/data_for_tests/Component/RouteLister/Controller/UserController.php
@@ -0,0 +1,10 @@
+<?php
+namespace tests_Ext_RouteLister\Controller;
+use DuckPhp\GlobalUser\UserControllerInterface;
+
+class UserController implements UserControllerInterface
+{
+    public function action_profile()
+    {
+    }
+}
