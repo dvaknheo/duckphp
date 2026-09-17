cfe9c6d608fb0cbbd6af20498ddeddb02c6f432e
dvaknheo <85811616@qq.com>
2026-09-17 21:18:13 +0800

    删除过时的 测试脚本

--- docker/test-php74/exec-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-if ! docker ps --format '{{.Names}}' | grep -q '^duckphp-test$'; then
-    echo "duckphp-test is not running, run ./start-docker.sh first"
-    exit 1
-fi
-
-docker exec -w /DATA duckphp-test "$@"
--- docker/test-php74/stop-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-if docker ps --format '{{.Names}}' | grep -q '^duckphp-test$'; then
-    docker stop duckphp-test
-    echo "duckphp-test stopped"
-else
-    echo "duckphp-test is not running"
-fi
