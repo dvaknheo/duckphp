cd1c9bf083258dd949d8dcafac34395ab5021f2b
dvaknheo <85811616@qq.com>
2026-09-24 18:00:41 +0800

    准备 DuckSystemException 的常量

--- src/Core/DuckPhpSystemException.php
+++ src/Core/DuckPhpSystemException.php
@@ -10,5 +10,12 @@ use Exception;
 
 class DuckPhpSystemException extends Exception
 {
-    //
+    // const E_NO_INIT_BASE_CLASS = -1;
+    // const E_Command_Not_Found_In_All = -2;
+    // const E_X = 'Command Need Parameter';
+    // const E_X2 = '"Phase Short name ({$this->phase_name}) is used';
+    // const E_X4  ='"ext [$class] not exists"';
+    // const E_C5 = 'Child [$class] not exists';
+    // const E_A1 = 'No GlobalAdmin Provider';
+    // const E_A2 = 'need ext options';
 }
